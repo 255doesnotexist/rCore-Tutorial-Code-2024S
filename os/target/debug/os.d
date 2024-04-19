42036028237961c6a9913d380301b79749238d8d
255doesnotexist <1293883574jcy@gmail.com>
2024-04-19 11:17:10 +0800

    main commit

---
+++ os/target/debug/os.d
@@ -0,0 +1,1 @@
+/home/ezra/rCore-Tutorial-Code-2024S/os/target/debug/os: /home/ezra/rCore-Tutorial-Code-2024S/os/src/main.rs
---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/log-b5d1d72b0507d41f.d
@@ -0,0 +1,10 @@
+/home/ezra/rCore-Tutorial-Code-2024S/os/target/riscv64gc-unknown-none-elf/release/deps/liblog-b5d1d72b0507d41f.rmeta: /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/lib.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/macros.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/serde.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/__private_api.rs
+
+/home/ezra/rCore-Tutorial-Code-2024S/os/target/riscv64gc-unknown-none-elf/release/deps/liblog-b5d1d72b0507d41f.rlib: /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/lib.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/macros.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/serde.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/__private_api.rs
+
+/home/ezra/rCore-Tutorial-Code-2024S/os/target/riscv64gc-unknown-none-elf/release/deps/log-b5d1d72b0507d41f.d: /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/lib.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/macros.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/serde.rs /home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/__private_api.rs
+
+/home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/lib.rs:
+/home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/macros.rs:
+/home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/serde.rs:
+/home/ezra/.cargo/registry/src/index.crates.io-6f17d22bba15001f/log-0.4.21/src/__private_api.rs:
